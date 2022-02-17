f22b3917e20a3e195e48cca93d16c7b5398e7a24
Swati Mandal <99341263+missaudio@users.noreply.github.com>
2022-02-17 21:49:28 +0530

    done

---
+++ pages/index.js
@@ -0,0 +1,240 @@
+
+
+export default function Home() {
+  return (
+    <div >
+      
+     <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+         
+          <span class="font-semibold text-xl tracking-tight">Open sea</span>
+        </div>
+        
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+         
+         
+        </div>
+      </nav>
+     <div class="grid grid-cols-3 lg:gap-3 justify-items-center">
+         <div class="py-10">
+       <div class="rounded overflow-hidden shadow-lg max-w-sm">
+           <img src="https://images.unsplash.com/photo-1577468845449-fe85c4725017?ixlib=rb-1.2.1{&}ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt="">
+           <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">mountain</div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+        </div>
+       </div>
+         </div>
+
+         <div class="py-10">
+            <div class="rounded overflow-hidden shadow-lg max-w-sm">
+                <img src="https://images.unsplash.com/photo-1643772526710-adf60ceaa303?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt=""/>
+  <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">mountain</div>
+     
+     
+     
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+
+    </div>
+            </div>
+              </div>
+
+              <div class="py-10">
+                <div class="rounded overflow-hidden shadow-lg max-w-sm">
+                    <img src="https://images.unsplash.com/photo-1643358044342-20bf6eaf8c55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt="">
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">mountain</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+                    </div>
+                </div>
+                  </div>
+     </div>
+
+     <div class="grid grid-cols-3 lg:gap-3 justify-items-center">
+        <div class="py-10">
+      <div class="rounded overflow-hidden shadow-lg max-w-sm">
+          <img src="https://images.unsplash.com/photo-1644122889873-b5d58109069f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM3fGFldTZyTC1qNmV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt=""/>
+          <div class="px-6 py-4">
+           <div class="font-bold text-xl mb-2">mountain</div>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+        </div>
+      </div>
+        </div>
+
+        <div class="py-10">
+           <div class="rounded overflow-hidden shadow-lg max-w-sm">
+               <img src="https://images.unsplash.com/photo-1643941217351-27a8f0978bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt=""/>
+ <div class="px-6 py-4">
+     <div class="font-bold text-xl mb-2">mountain</div>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+    </div>
+           </div>
+             </div>
+
+             <div class="py-10">
+               <div class="rounded overflow-hidden shadow-lg max-w-sm">
+                   <img src="https://images.unsplash.com/photo-1644258676710-ffb99d7d7a1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI3fGFldTZyTC1qNmV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt=""/>
+                   <div class="px-6 py-4">
+                       <div class="font-bold text-xl mb-2">mountain</div>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+                       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+                    </div>
+               </div>
+                 </div>
+    </div>
+
+    <div class="grid grid-cols-3 lg:gap-3 justify-items-center">
+        <div class="py-10">
+      <div class="rounded overflow-hidden shadow-lg max-w-sm">
+          <img src="daniel-sessler-ooI4bOT95Ow-unsplash.jpg" alt=""/>
+          <div class="px-6 py-4">
+           <div class="font-bold text-xl mb-2">mountain</div>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+        </div>
+      </div>
+        </div>
+
+        <div class="py-10">
+           <div class="rounded overflow-hidden shadow-lg max-w-sm">
+               <img src="daniel-sessler-ooI4bOT95Ow-unsplash.jpg" alt=""/>
+ <div class="px-6 py-4">
+     <div class="font-bold text-xl mb-2">mountain</div>
+    
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+    </div>
+           </div>
+             </div>
+
+             <div class="py-10">
+               <div class="rounded overflow-hidden shadow-lg max-w-sm">
+                   <img src="https://images.unsplash.com/photo-1643771905812-b2fe27c08574?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt=""/>
+                   <div class="px-6 py-4">
+                       <div class="font-bold text-xl mb-2">mountain</div>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+                       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+                  
+                    </div>
+               </div>
+                 </div>
+    </div>
+
+    <div class="grid grid-cols-3 lg:gap-3 justify-items-center">
+        <div class="py-10">
+      <div class="rounded overflow-hidden shadow-lg max-w-sm">
+          <img src="daniel-sessler-ooI4bOT95Ow-unsplash.jpg" alt="">
+          <div class="px-6 py-4">
+           <div class="font-bold text-xl mb-2">mountain</div>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+        </div>
+      </div>
+        </div>
+
+        <div class="py-10">
+           <div class="rounded overflow-hidden shadow-lg max-w-sm">
+               <img src="daniel-sessler-ooI4bOT95Ow-unsplash.jpg" alt=""/>
+ <div class="px-6 py-4">
+     <div class="font-bold text-xl mb-2">mountain</div>
+     
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+    </div>
+           </div>
+             </div>
+
+             <div class="py-10">
+               <div class="rounded overflow-hidden shadow-lg max-w-sm">
+                   <img src="daniel-sessler-ooI4bOT95Ow-unsplash.jpg" alt="">
+                   <div class="px-6 py-4">
+                       <div class="font-bold text-xl mb-2">mountain</div>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+                       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+                    </div>
+               </div>
+                 </div>
+    </div>
+
+    <div class="grid grid-cols-3 lg:gap-3 justify-items-center">
+       <div class="py-10">
+     <div class="rounded overflow-hidden shadow-lg max-w-sm">
+         <img src="https://images.unsplash.com/photo-1644122889873-b5d58109069f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM3fGFldTZyTC1qNmV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="">
+         <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">mountain</div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+        </div>
+     </div>
+       </div>
+
+       <div class="py-10">
+          <div class="rounded overflow-hidden shadow-lg max-w-sm">
+              <img src="https://images.unsplash.com/photo-1643941217351-27a8f0978bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+<div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">mountain</div>
+   
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+ <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+</div>
+          </div>
+            </div>
+
+            <div class="py-10">
+              <div class="rounded overflow-hidden shadow-lg max-w-sm">
+                  <img src="https://images.unsplash.com/photo-1644258676710-ffb99d7d7a1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI3fGFldTZyTC1qNmV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="">
+                  <div class="px-6 py-4">
+                      <div class="font-bold text-xl mb-2">mountain</div>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+                      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+                    </div>
+              </div>
+                </div>
+   </div>
+
+   <div class="grid grid-cols-3 lg:gap-3 justify-items-center">
+       <div class="py-10">
+     <div class="rounded overflow-hidden shadow-lg max-w-sm">
+         <img src="https://images.unsplash.com/photo-1644623532454-116f86557331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt="">
+         <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">mountain</div>
+         
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+        </div>
+     </div>
+       </div>
+
+       <div class="py-10">
+          <div class="rounded overflow-hidden shadow-lg max-w-sm">
+              <img src="https://images.unsplash.com/photo-1603573608528-62d6902517c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" alt="">
+<div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">mountain</div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+</div>
+          </div>
+            </div>
+
+            <div class="py-10">
+              <div class="rounded overflow-hidden shadow-lg max-w-sm">
+                  <img src="https://images.unsplash.com/photo-1643771905812-b2fe27c08574?ixlib=rb-1.2.1 &ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt="" />
+                  <div class="px-6 py-4">
+                      <div class="font-bold text-xl mb-2">mountain</div>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis culpa doloremque perferendis mollitia repellendus, ab, minus laborum possimus voluptas dolor. Doloribus itaque laboriosam, mollitia et dicta distinctio voluptatibus!</p>
+                      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read more</button>
+                  </div>
+              </div>
+                </div>
+   </div>
+
+    </div>
+  )
+}
